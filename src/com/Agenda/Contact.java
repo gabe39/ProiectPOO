3063439c3ce2f6fcb330bf1de2c95a865cb9574c
Gabe <gabigb39@yahoo.com>
2022-03-29 19:02:09 +0300

    Initial commit

---
+++ src/com/Agenda/Contact.java
@@ -0,0 +1,7 @@
+package com.Agenda;
+
+public class Contact
+{
+    public String nume,prenume,adresa;
+    public int nrTelefon,nrTelefonAcasa;
+}
---
+++ src/com/Agenda/Main.java
@@ -0,0 +1,12 @@
+package com.Agenda;
+import java.util.*;
+
+public class Main {
+
+    public List<Contact> contacte = new ArrayList<Contact>();
+
+    public static void main(String[] args)
+    {
+
+    }
+}
