09fbb68697ade4569aac6dfc8d3236064173ea52
Gabe <gabigb39@yahoo.com>
2022-03-31 08:04:25 +0300

    Added NewUser page

--- src/com/Agenda/Main.java
+++ src/com/Agenda/Main.java
@@ -4,10 +4,13 @@ import java.util.*;
 public class Main {
 
     public List<Contact> contacte = new ArrayList<Contact>();
+    public static Scanner scn = new Scanner(System.in);
+    public static String numeUtilizator;
 
     public static void main(String[] args)
     {
-
+        boolean utilizatorNou = true;
+        if(utilizatorNou) PageUtilizatorNou();
     }
 
     //region print
@@ -16,4 +19,24 @@ public class Main {
         System.out.print(obj);
     }
     //endregion
+
+    //region Clear Screen Function
+    public static void clrscr()
+    {
+        print("\033[H\033[2J");
+        System.out.flush();
+    }
+    //endregion
+
+    public static void MainPage()
+    {
+
+    }
+
+    public static void PageUtilizatorNou()
+    {
+        print("Bine ai venit!");
+        print("\nEste prima data cand deschizi aceasta aplicatie, introdu numele detinatorului acestei agende electronice:\n");
+        numeUtilizator = scn.next();
+    }
 }
