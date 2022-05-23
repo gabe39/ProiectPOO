d254e363d96d0a0bb08e1c2a2b562d816ecc435d
Gabe <gabigb39@yahoo.com>
2022-05-23 19:50:56 +0300

    Butoane lista sarcina

--- AgendaFX2/src/sample/FXML_Pages/AdaugaSarcinaPage.java
+++ AgendaFX2/src/sample/FXML_Pages/AdaugaSarcinaPage.java
@@ -5,12 +5,19 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import sample.FXMLSceneChanger;
+import sample.Sarcina;
 
 public class AdaugaSarcinaPage {
 
     @FXML AnchorPane rootPane;
     @FXML TextField tfSarcina;
     @FXML DatePicker dpTermenLimita;
+
+    public void AdaugaSarcina() throws Exception {
+        String tLimita = dpTermenLimita.toString();
+        Sarcina s = new Sarcina(tfSarcina.getText(), tLimita, false);
+        new FXMLSceneChanger().SwitchScene("FXML_Pages/Main", rootPane.getScene());
+    }
 
     public void GoToBack() throws Exception {
         new FXMLSceneChanger().SwitchScene("FXML_Pages/ListaSarciniPage", rootPane.getScene());
